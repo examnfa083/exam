3a0f624cb6ded53a8d38924f03d6835763de2d23
Charles Casassus <charles.casassus@gmail.com>
2018-06-13 10:57:50 +0200

    correction requète

--- inc/question-insert.inc.php
+++ inc/question-insert.inc.php
@@ -3,7 +3,7 @@
 /* RECUP USER LISTE ============================================================ */
     if(isset($question_pseudo)){                                  /* uniquement si non vide */
         $requete21="INSERT INTO question(question_id, question_pseudo, question_texte) 
-                   VALUES ('','.$question_pseudo.','.$question_texte.')";
+                   VALUES ('','$question_pseudo','$question_texte')";
         /* DEBUG */ echo "requete21 : ".$requete21.'<br />';
         $resultat21 = $con->query($requete21);           /* Pour préparer et executer la requete */
     } ?>
